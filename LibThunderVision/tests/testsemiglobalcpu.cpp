3d0e5f92e497ffb98fa373e3dab89e4382db9efb
Otavio BG <otavio.b.gomes@gmail.com>
2011-06-12 22:01:00 -0300

    added semiglobal test on cpu

---
+++ LibThunderVision/tests/testsemiglobalcpu.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <tdvision/ssddev.hpp>
+#include <tdvision/dynamicprogdev.hpp>
+#include <tdvision/semiglobalcpu.hpp>
+#include <tdvision/wtacpu.hpp>
+#include "stereotest.hpp"
+
+TEST(TestSemiGlobalCPU, WithSSD)
+{
+    tdv::SSDDev cost(64);
+    tdv::SemiGlobalCPU sg;
+    
+   runStereoTest(
+#if 0
+       "../../res/tsukuba512_L.png",
+       "../../res/tsukuba512_R.png",
+#else
+       "q_left.png",
+       "q_right.png",
+#endif
+       "tsukuba_ssdsgcpu.png",
+       &cost, &sg, true);
+}
